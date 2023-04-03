9b3ddd5a0f6ebc4dabeac4f1c6efb438888f6033
wocjs <wocjs602@gmail.com>
2023-04-03 22:49:56 +0900

    14003 성고옹~

---
+++ Code/14003/14003_H.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('init.txt', 'r')
+#####
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+#####
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [1]
+x = [arr[0]]
+res = [1]
+for i in range(1, n):
+    if arr[i] > x[-1]:
+        x.append(arr[i])
+        dp.append(dp[-1] + 1)
+        res.append(len(dp))
+    else:
+        idx = bisect_left(x, arr[i])    # 이진탐색, 인덱스 반환
+        x[idx] = arr[i]
+        res.append(idx+1)
+print(dp[-1])
+
+ans = []
+i = dp[-1]
+pt = n-1
+while i > 0:
+    if res[pt] == i:
+        ans.append(arr[pt])
+        i -= 1
+    pt -= 1
+print(*ans[::-1])
